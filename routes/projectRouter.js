a3f27d755143230c41e64d85227668cffe825d2d
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2020-01-26 19:30:26 -0800

    Add getProjectActions

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -24,7 +24,16 @@ router.get('/:id', validateId, (req, res) => {
         .catch(err => res.status(500).json(err.message))
 })
 
-
+router.get('/:id/actions', validateId, (req, res) => {
+    const {id} = req.params;
+    Projects.getProjectActions(id)
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            res.status(500).json(err.message);
+        })
+})
 
 
 
