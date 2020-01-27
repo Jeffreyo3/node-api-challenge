e38df53254b615ff76152406b2d9a605dfc6c5ed
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2020-01-26 19:11:37 -0800

    Create routes
    Update server.js typo
    
    Add .get() to action & projectRotuers

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 Go code!
 */
 
-const server = require("./server");
+require('dotenv').config();
+const server = require('./server.js');
 
 const port = process.env.PORT || 4000;
-
-server.listen(port, () => console.log(`*** Server is running on http://localhost:${port} ***`))
+server.listen(port, () => {
+    console.log(`**** Listening On Port http://localhost:${port}****`)
+});
---
+++ routes/actionRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Projects = require('../data/helpers/projectModel');
+const Actions = require('../data/helpers/actionModel');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(list => {
+            if (list) {
+                res.status(200).json(list);
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err.message);
+        });
+});
+
+module.exports = router;
